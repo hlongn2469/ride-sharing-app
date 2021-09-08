7e718f9f9116eb741c329a6027db794b778409a5
Kray Nguyen <64120863+krayn1@users.noreply.github.com>
2021-09-08 00:23:08 -0700

    initial commit

---
+++ server/taxi/routing.py
@@ -0,0 +1,18 @@
+# server/taxi/routing.py
+
+from django.core.asgi import get_asgi_application
+from django.urls import path
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from taxi.middleware import TokenAuthMiddlewareStack # new
+from trips.consumers import TaxiConsumer
+
+
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': TokenAuthMiddlewareStack( # changed
+        URLRouter([
+            path('taxi/', TaxiConsumer.as_asgi()),
+        ])
+    ),
+})
